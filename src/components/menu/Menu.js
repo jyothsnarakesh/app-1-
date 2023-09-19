4a24dbc3ca5f5f0d6ddce7504a060e40839dada9
jyothsnarakesh <65153436+jyothsnarakesh@users.noreply.github.com>
2023-09-19 16:28:25 +0530

    Menu css changes

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,20 @@
+import Home from '@/app/page'
+import Link from 'next/link'
 import React from 'react'
+import styles from "./Menu.module.css";
 
 export const Menu = () => {
-  return (
-    <div>Menu</div>
-  )
+  return <ul className={styles.menu}>
+    <li className={styles.menuItem}>
+      <Link href="/home">Home</Link>
+    </li>
+    <li className={styles.menuItem}>      
+      <Link href="/about">About</Link>
+    </li>
+    <li className={styles.menuItem}>
+      <Link href="/contact">Contact</Link>
+    </li>
+  </ul>
+
+  
 }
